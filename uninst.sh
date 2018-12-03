b137a6e2c07e09c7b0a858afe6fac6f3e46bb88b
Germano Gualberto <gmanopoliano@gmail.com>
2018-12-03 10:58:10 -0300

    uninst pronto

--- uninst.sh
+++ uninst.sh
@@ -1,42 +1,28 @@
 #desinstala a aplicacao
 
-#instala aplicacao
 #verificar se ja está instalado
-
-#funcao que copia arquivos de . para /usr/bin
-#copyFiles(){
-#	echo "arquivos copiados"
-#}
 
 OLD_NAME='/usr/bin/old-lp'
 NAME='/usr/bin/lp'
 NEW_NAME='/usr/bin/new-lp.sh'
 DIR='/usr/bin/'
 
-#salvar valor cota se tiver
-
-#echo $# " argumentos passados"
-if [ $# -gt 0 ];
-then
-	#echo "nova cota: " $1
-	#cat cota.info
-	echo $1 > cota.info
-	#cat cota.info
-fi
-
 #renomear antigo
 
-if [ ! -f $OLD_NAME ];
+if [ -f $OLD_NAME ];
 then
-	echo "nao existe old-lp"
-	#copia arquivos
-#	copyFiles
-	cp -R . $DIR
+	echo "existe old-lp"
+	#apaga arquivos
+	rm -rf $DIR+'new-lp-doc'
+	rm -rf $DIR+'new-lp-scripts'
+	rm -rf $DIR+'README.md'
+	rm -rf $DIR+'inst.sh'
+	rm -rf $DIR+'uninst.sh'
+	rm -rf $DIR+'lp'
+	echo "arquivos apagados"
 	#renomear antigo
-	mv $NAME $OLD_NAME
-	#renomear novo
-	mv $NEW_NAME $NAME
+	mv $OLD_NAME $NAME
 
 else
-	echo "instalação prévia do new-lp encontrada"
+	echo "instalação prévia do new-lp nao encontrada"
 fi
